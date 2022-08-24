5bb099c70457f9ce21504225d62c537839782b97
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-08-24 21:47:53 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,132 @@
+//find the mistakes from the code
+///find the code to welcome the boss
+///find the code to welcome the boss
+// let login = 'Employee';
+// let message = (login == 'Employee') ? :
+//   (login == 'Director') ? 'Greetings' :
+//   (login == '') ? 'No login' :
+//   '';
+// console.log(message);
+
+let login = 'Employee';
+let message = (login === 'Employee') ? 
+  (login === 'Director') : 'Greetings' ?
+  (login == '') : 'No login' 
+console.log(message);
+
+
+
+///find the code to welcome the boss
+
+// You cant change the value of the msg
+// let message;
+// if (null || 2 || undefined )
+// {
+//  let message = "welcome boss";
+// }
+// else
+// {
+//  let message = "Go away";
+// }
+//   console.log(message);
+
+
+// You cant change the value of the msg
+var message;
+if (null || 2 || undefined )
+{
+    
+    let message = "welcome boss";
+    console.log(message);
+}
+
+
+
+else
+{
+ let message = "Go away";
+
+  console.log(message);
+}
+
+
+//3rd one
+let message;
+let lock = 2;
+//Dont change any code below this
+if (lock && " " || undefined )
+{
+  message = "Go away";
+}
+else
+{
+ message = "welcome";
+}
+console.log(message);
+
+
+//4th
+//You can change only 2 characters
+let i = 3;
+while (i) {
+  console.log( "print" );
+}
+
+
+
+//5th
+//You can change only 2 characters
+let num = 10;
+num.map((num)=>
+       {
+    console.log(num[i]);
+  
+});
+  
+
+//6th
+let gifts = ["teddy bear", "drone", "doll"];
+gifts.map((gift)=>{
+  console.log('Wrapped ${`gifts[i]`} and added a bow!');
+});
+
+
+
+//7th
+for (let num = 2; num % 2===0; num += 1) {
+    console.log(num);
+  }
+
+
+  //8th
+  let countdown = 100;
+while (countdown == 0) {
+  countdown--;
+  if(countdown >0)
+  {
+   console.log("bomb triggered");
+  }
+}
+
+
+
+// //9th
+// Whats the msg printed and why?
+
+// var lemein = “0”;
+// var lemeout = 0;
+// var msg = “”;
+// if (lemein) {
+//  msg += “hi”;
+//  }
+// if (lemeout) {
+//  msg += ‘Hello’;
+// }
+// console.log(msg);
+
+
+//solution-10hi
+//2)hello
+//3)""
+
+
